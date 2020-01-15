c609838a0a6567df14d4d5194d628aed48ee63c1
Andrew Bishay <50244538+bbishay@users.noreply.github.com>
2020-01-14 21:02:36 -0500

    finished

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const logger = require('./middleware/logger')
+const exphbs = require('express-handlebars');
+//const logger = require('./middleware/logger');
 
 /* app.get('/', (req, res) => { 
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -11,9 +12,19 @@ const logger = require('./middleware/logger')
 //Init middleware 
 //app.use(logger);
 
+//handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
 //Body Parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+
+//Homepage route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App'
+}));
 
 //Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
